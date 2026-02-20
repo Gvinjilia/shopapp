405a157542cdb6c39c7ebc688b0a2c61e4050fc1
Gvinjilia <gvinjilian2@gmail.com>
2026-02-21 00:49:19 +0400

    updated server app.js file

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(morgan('dev'))
 app.use(cors({
-    origin: ["*", 'http://localhost:5173'],
+    origin: ["*", 'http://localhost:5173', 'https://shopapp-indol.vercel.app'],
     credentials: true
 }));
 app.use(cookieParser());
